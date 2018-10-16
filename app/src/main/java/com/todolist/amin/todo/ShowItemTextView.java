5f0b8379200089c508779400261cf923f87f3a58
amin <aminullahtajmuhammad@gmail.com>
2018-10-16 23:17:27 +0500

    Add TextInputLayout in Main Activity and Add singleton class

--- app/src/main/java/com/todolist/amin/todo/ShowItemTextView.java
+++ app/src/main/java/com/todolist/amin/todo/ShowItemTextView.java
@@ -16,6 +16,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ZoomButtonsController;
 
 import java.io.Serializable;
@@ -23,10 +24,12 @@ import java.util.ArrayList;
 
 public class ShowItemTextView extends AppCompatActivity {
     EditText showEditView;
+    EditText editSubTask;
     ImageButton btnSave;
+    Button btn_Subtask;
     String show_text;
-    String editedData;
-    ArrayList<String> subTasks;
+    String editedData, str_SubTask;
+    ArrayList<String> subTasks = new ArrayList<>();
     AdapterSubTasks adapterSubTasks;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +37,7 @@ public class ShowItemTextView extends AppCompatActivity {
         setContentView(R.layout.activity_show_item_text_view);
         showEditView = findViewById(R.id.tv_showText);
         btnSave = findViewById(R.id.save_txt_button);
+        btn_Subtask = findViewById(R.id.btn_subtask);
 
         final Intent intent;
         intent = getIntent();
@@ -41,9 +45,13 @@ public class ShowItemTextView extends AppCompatActivity {
         showEditView.setText(show_text);
         btnSave.setVisibility(View.INVISIBLE);
         setToolBar();
-        setAnimationOfSubtask();
-        setRecyclerViewForSubTask();
 
+        btn_Subtask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setRecyclerViewForSubTask();
+            }
+        });
     }
     void setToolBar() {
         ImageButton btnBack = findViewById(R.id.btnBack);
@@ -51,6 +59,9 @@ public class ShowItemTextView extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 editedData = showEditView.getText().toString();
+                Toast.makeText(ShowItemTextView.this, "Your New Text is Saved",
+                        Toast.LENGTH_SHORT).show();
+
                 Intent intent1 = new Intent();
                 intent1.putExtra("Edited_Data",editedData);
                 setResult(Activity.RESULT_OK,intent1);
@@ -62,23 +73,19 @@ public class ShowItemTextView extends AppCompatActivity {
         txtAppName.setVisibility(View.GONE);
 
     }
-    void setAnimationOfSubtask() {
-        Button btn_Subtask = findViewById(R.id.btn_subtask);
-        final EditText editText = findViewById(R.id.edt_subtask);
-        btn_Subtask.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-    }
     void setRecyclerViewForSubTask() {
         RecyclerView recyclerView = findViewById(R.id.subtask_recycler);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        editSubTask = findViewById(R.id.edt_subtask);
+        str_SubTask = editSubTask.getText().toString();
         adapterSubTasks = new AdapterSubTasks(subTasks);
+        adapterSubTasks.addItem(str_SubTask);
+        adapterSubTasks.notifyItemInserted(adapterSubTasks.getItemCount());
         recyclerView.setAdapter(adapterSubTasks);
+        editSubTask.setText("");
     }
+
 }
